106e64735193ae3a34b856ae731f5a3710f5f332
johnboi33 <ogundarej55@gmail.com>
2026-04-22 07:41:38 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.com/johnboi33/emperor-blog',
+	base: '/my-repo',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
